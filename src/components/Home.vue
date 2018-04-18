d4e4f1972d81fb953925ba2246f5f81238e05108
Arnav Gupta <arnav@codingblocks.com>
2018-04-18 21:49:29 +0530

    sidedrawer for both platforms
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,26 @@
 <template>
   <Page class="page">
-    <ActionBar class="action-bar" title="Home"/>
+    <ActionBar android.systemIcon="ic_menu_back" class="action-bar" title="Home">
+      <NavigationButton
+              v-if="$isAndroid"
+              text="Go Back"
+              icon="res://ic_menu_white_24dp"
+              @tap="$refs.drawer.nativeView.showDrawer()"/>
+      <ActionItem
+              v-else
+              text="Menu"
+              ios.position="left"
+              icon="res://ic_menu"
+              @tap="$refs.drawer.nativeView.showDrawer()" />
+    </ActionBar>
 
     <RadSideDrawer id="drawer" ref="drawer" showOverNavigation="true">
-      <StackLayout  background="white" ~drawerContent>
+      <StackLayout class="drawer-content"  background="white" ~drawerContent>
         <StackLayout class="sidedrawer-header" background="#E27834"></StackLayout>
         <StackLayout >
-          <TextView class="sidedrawer-list-item">Events</TextView>
-          <TextView class="sidedrawer-list-item">Conferences</TextView>
-          <TextView class="sidedrawer-list-item">Talkfunnel</TextView>
+          <Label class="sidedrawer-list-item">Events</Label>
+          <Label class="sidedrawer-list-item">Conferences</Label>
+          <Label class="sidedrawer-list-item">Talkfunnel</Label>
         </StackLayout>
       </StackLayout>
       <StackLayout ~mainContent>
@@ -19,11 +31,39 @@
 
   </Page>
 </template>
-<style>
+<script>
+  export default {
+
+    mounted: function () {
+      // Add shadow to iOS Drawer
+      if(this.$refs.drawer._nativeView.ios)  {
+        const ios = this.$refs.drawer._nativeView.ios
+        // .. but if the menu is drawn 'above' the hostview, do this:
+        ios.defaultSideDrawer.style.shadowMode = 2; // TKSideDrawerShadowMode.SideDrawer;
+        // if you have shadowMode = 2, then you can add a little dim to the lower layer to add some depth. Keep it subtle though:
+        ios.defaultSideDrawer.style.dimOpacity = 0.12;
+
+        // then tweak the shadow to your liking:
+        ios.defaultSideDrawer.style.shadowOpacity = 0.75; // 0-1, higher is darker
+        ios.defaultSideDrawer.style.shadowRadius = 5; // higher is more spread
+
+      }
+    }
+
+  }
+</script>
+<style scoped>
   #drawer > StackLayout {
     height: 100%;
   }
   .sidedrawer-list-item {
     padding-left: 10;
   }
+
+  #drawer Label {
+    font-family: sans-serif-thin;
+    padding-left: 20;
+    padding-top: 10;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'nativescript-vue';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
+import { isAndroid, isIOS } from 'tns-core-modules/platform';
+
+Vue.prototype.$isAndroid = isAndroid;
+Vue.prototype.$isIOS = isIOS;
 
 Vue.registerElement('RadSideDrawer', () => RadSideDrawer)
 
@@ -10,7 +14,7 @@ import store from './store';
 import './styles.scss';
 
 // Uncommment the following to see NativeScript-Vue output logs
-//Vue.config.silent = false;
+Vue.config.silent = false;
 
 new Vue({
 
