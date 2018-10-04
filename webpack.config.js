da18555534fccc3043868605dca308ee88234fc4
Arnav Gupta <arnav@codingblocks.com>
2018-10-04 22:17:43 +0530

    resolve main nicely
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports = env => {
     const appResourcesFullPath = resolve(projectRoot, appResourcesPath);
 
     const entryModule = nsWebpack.getEntryModule(appFullPath);
-    const entryPath = `.${sep}${entryModule}.ts`;
+    const entryPath = `.${sep}${entryModule}`;
     console.log(`Bundling application for entryPath ${entryPath}...`);
 
     const config = {
